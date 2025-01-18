12ca7d3f7f0529473667212cfae695ef95b7be6f
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2025-01-18 10:53:08 -0300

    chore: set up bga-zoom

--- propuh.js
+++ propuh.js
@@ -10,93 +10,97 @@
  * propuh.js
  *
  * propuh user interface script
- * 
+ *
  * In this file, you are describing the logic of your user interface, in Javascript language.
  *
  */
 
 define([
-    "dojo","dojo/_base/declare",
-    "ebg/core/gamegui",
-    "ebg/counter"
-],
-function (dojo, declare) {
-    return declare("bgagame.propuh", ebg.core.gamegui, {
-        constructor: function(){
-            console.log('propuh constructor');
+  "dojo",
+  "dojo/_base/declare",
+  "ebg/core/gamegui",
+  "ebg/counter",
+  `${g_gamethemeurl}modules/js/bga-zoom.js`,
+], function (dojo, declare) {
+  return declare("bgagame.propuh", ebg.core.gamegui, {
+    constructor: function () {
+      console.log("propuh constructor");
+    },
+
+    setup: function (gamedatas) {
+      console.log("Starting game setup");
+
+      this.pph = {
+        managers: {},
+        info: {},
+        globals: {},
+      };
+
+      this.pph.managers.zoom = new ZoomManager({
+        element: document.getElementById("pph_gameArea"),
+        localStorageZoomKey: "propuh-zoom",
+        zoomControls: {
+          color: "black",
         },
-        
-        setup: function( gamedatas )
-        {
-            console.log( "Starting game setup" );
-            
- 
-            // Setup game notifications to handle (see "setupNotifications" method below)
-            this.setupNotifications();
+        zoomLevels: [0.5, 0.75, 1, 1.25, 1.5],
+        smooth: true,
+        onZoomChange: (zoom) => {},
+      });
 
-            console.log( "Ending game setup" );
-        },
-       
+      // Setup game notifications to handle (see "setupNotifications" method below)
+      this.setupNotifications();
 
-        ///////////////////////////////////////////////////
-        //// Game & client states
-        
-        // onEnteringState: this method is called each time we are entering into a new game state.
-        //                  You can use this method to perform some user interface changes at this moment.
-        //
-        onEnteringState: function( stateName, args )
-        {
-            console.log( 'Entering state: '+stateName, args );
-            
-            switch( stateName )
-            {
-            
-            case 'dummy':
-                break;
-            }
-        },
+      console.log("Ending game setup");
+    },
 
-        // onLeavingState: this method is called each time we are leaving a game state.
-        //                 You can use this method to perform some user interface changes at this moment.
-        //
-        onLeavingState: function( stateName )
-        {
-            console.log( 'Leaving state: '+stateName );
-            
-            switch( stateName )
-            {
-           
-            case 'dummy':
-                break;
-            }               
-        }, 
+    ///////////////////////////////////////////////////
+    //// Game & client states
 
-        // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
-        //                        action status bar (ie: the HTML links in the status bar).
-        //        
-        onUpdateActionButtons: function( stateName, args )
-        {
-            console.log( 'onUpdateActionButtons: '+stateName, args );
-        },        
+    // onEnteringState: this method is called each time we are entering into a new game state.
+    //                  You can use this method to perform some user interface changes at this moment.
+    //
+    onEnteringState: function (stateName, args) {
+      console.log("Entering state: " + stateName, args);
 
-        ///////////////////////////////////////////////////
-        //// Utility methods
+      switch (stateName) {
+        case "dummy":
+          break;
+      }
+    },
 
+    // onLeavingState: this method is called each time we are leaving a game state.
+    //                 You can use this method to perform some user interface changes at this moment.
+    //
+    onLeavingState: function (stateName) {
+      console.log("Leaving state: " + stateName);
 
-        ///////////////////////////////////////////////////
-        //// Player's action
-        
-        onCardClick: function( card_id )
-        {
-            console.log( 'onCardClick', card_id );       
-        },    
+      switch (stateName) {
+        case "dummy":
+          break;
+      }
+    },
 
-        
-        ///////////////////////////////////////////////////
-        //// Reaction to cometD notifications
-        setupNotifications: function()
-        {
-            console.log( 'notifications subscriptions setup' );
-        },  
-   });             
+    // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
+    //                        action status bar (ie: the HTML links in the status bar).
+    //
+    onUpdateActionButtons: function (stateName, args) {
+      console.log("onUpdateActionButtons: " + stateName, args);
+    },
+
+    ///////////////////////////////////////////////////
+    //// Utility methods
+
+    ///////////////////////////////////////////////////
+    //// Player's action
+
+    onCardClick: function (card_id) {
+      console.log("onCardClick", card_id);
+    },
+
+    ///////////////////////////////////////////////////
+    //// Reaction to cometD notifications
+    setupNotifications: function () {
+      console.log("notifications subscriptions setup");
+    },
+  });
 });
